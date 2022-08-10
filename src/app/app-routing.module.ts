8674d083901c82cc68e1bad2aae590983259ef27
didier01 <deramirez19@misena.edu.co>
2022-08-10 14:46:28 -0500

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComingSoonComponent } from './views/coming-soon/coming-soon.component';
+import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ComingSoonComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComingSoonComponent } from './views/coming-soon/coming-soon.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComingSoonComponent
+    ComingSoonComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
